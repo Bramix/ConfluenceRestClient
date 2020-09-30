07c168f94a71db9b0631d674813fd341f386288d
vlbo0819 <vladyslav.bozhenko@netcracker.com>
2020-09-30 11:35:55 +0300

    Expandable field, Maven build configuration

--- src/main/java/de/micromata/confluence/rest/core/domain/BaseBean.java
+++ src/main/java/de/micromata/confluence/rest/core/domain/BaseBean.java
@@ -3,6 +3,8 @@ package de.micromata.confluence.rest.core.domain;
 import com.google.gson.annotations.Expose;
 import de.micromata.confluence.rest.core.domain.common.LinksBean;
 
+import java.util.Map;
+
 /**
  * Author: Christian Schulze (c.schulze@micromata.de)
  * Date: 02.07.2016
@@ -18,6 +20,9 @@ public class BaseBean {
 
     @Expose
     private LinksBean links;
+
+    @Expose
+    private Map<String, String> _expandable;
 
     public Integer getId() {
         return id;
@@ -41,5 +46,13 @@ public class BaseBean {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public Map<String, String> getExpandable() {
+        return _expandable;
+    }
+
+    public void setExpandable(Map<String, String> _expandable) {
+        this._expandable = _expandable;
     }
 }
